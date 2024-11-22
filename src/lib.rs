27d4b91eb450abc5c29ee1717f3c1d696f426e44
laxpsy <gujralanshg@gmail.com>
2024-11-23 02:18:43 +0530

    refactor: lib.rs, simplified main. chapter-3

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
+use std::error::{self, Error};
+use std::io::{Read, Write};
 use std::process;
 use std::{io, mem};
 
-use libc::{tcgetattr, tcsetattr, termios as Termios, ECHO, ICANON, TCSAFLUSH};
+use libc::{
+    tcgetattr, tcsetattr, termios as Termios, write, ECHO, ICANON, ICRNL, ISIG, IXON, OPOST,
+    TCSAFLUSH, VMIN, VTIME,
+};
 
 // TODO: Rewrite with Mutexes and replace static mut.
 static mut ORIGINAL_TERMIOS: Termios = unsafe { mem::zeroed() };
 
 /*
   Enable Raw Mode for Terminal.
+  ICRNL: disable translation of \r to \n.
+  IXON: disable XON, XOFF
+  ICANON: disable canonical mode
+  ECHO: disable echoing of STDDIN
+  ISIG: disable signal interrupts
+  OPOST: disable output post-processing
+        i.e conversion of /n to /r/n.
+  VMIN: how many bytes in input buffer?
+  VTIME: timeout in tenth-of-seconds
 */
 pub fn enable_raw_mode() {
     let mut termios = get_terminal_attributes().unwrap_or_else(|err| {
@@ -15,7 +29,11 @@ pub fn enable_raw_mode() {
         process::exit(1);
     });
     store_original_attributes(&termios);
-    termios.c_lflag &= !(ECHO | ICANON);
+    termios.c_iflag &= !(ICRNL | IXON);
+    termios.c_oflag &= !(OPOST);
+    termios.c_lflag &= !(ECHO | ICANON | ISIG);
+    termios.c_cc[VMIN] = 1;
+    termios.c_cc[VTIME] = 0;
     set_terminal_attributes(&termios).unwrap_or_else(|err| {
         eprintln!("Could not set terminal attributes. Program will now exit. {err}");
         process::exit(1);
@@ -73,6 +91,53 @@ fn wrap_with_result(result: i32) -> io::Result<()> {
     }
 }
 
+fn editor_read_key() -> [u8; 1] {
+    let mut buffer = [0; 1];
+    io::stdin().read_exact(&mut buffer).unwrap_or_else(|err| {
+        eprintln!("Failure in reading input from the user. Program will now exit! {err}");
+        process::exit(1);
+    });
+    buffer
+}
+
+/*
+    Handle quitting with Ctrl+Q.
+    Note to self - add BITMASK logic
+    for control characters.
+    Instead of taking in RawModeGuard
+    as a parameter, handle the case with
+    Errors.
+*/
+pub fn editor_process_key() -> Result<(), ()> {
+    const CTRL_Q: u8 = 0b00010001;
+    let character_u8 = editor_read_key()[0];
+    if character_u8 <= 31 || character_u8 == 127 {
+        print!("{character_u8}\r\n");
+    } else {
+        print!("{0}, {1}\r\n", character_u8, character_u8 as char);
+    }
+    io::stdout().flush().unwrap();
+    if character_u8 == CTRL_Q {
+        return Err(());
+    }
+    Ok(())
+}
+
+/*
+    Grab a lock to STDOUT stream,
+    write byte \x1b[2J to clear screen
+    and \x1b[H to reset cursor to
+    original position.
+    VT100 terminal commands.
+*/
+
+pub fn editor_refresh_screen() -> () {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    handle.write_all(b"\x1b[2J\x1b[H").unwrap();
+    handle.flush().unwrap();
+}
+
 /*
   Implementing RAII pattern to ensure Raw Mode
   is disabled when the program exits.
@@ -88,6 +153,9 @@ impl RawModeGuard {
 
 impl Drop for RawModeGuard {
     fn drop(&mut self) {
-        disable_raw_mode();
+        disable_raw_mode().unwrap_or_else(|err| {
+            eprintln!("ERROR! Could not restore Terminal Attributes. {err}");
+            process::exit(1);
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
-use rust_kilo::RawModeGuard;
-use std::io::{self, stdin, Read};
+use rust_kilo::{editor_process_key, editor_refresh_screen, RawModeGuard};
+use std::io::{self, Read};
 
 fn main() {
     let _raw_mode = RawModeGuard::new();
-    println!("Raw mode enabled. Press any key...");
+    print!("Raw mode enabled. Press any key...\r\n");
 
-    for byte in io::stdin().bytes() {
-        let byte = byte.unwrap();
-        let character = byte as char;
-        if character.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", byte);
-        } else {
-            println!(
-                "Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",
-                byte, character
-            );
-        }
-
-        if character == 'q' {
-            drop(_raw_mode);
-            break;
+    loop {
+        //  editor_refresh_screen();
+        match editor_process_key() {
+            Ok(()) => continue,
+            Err(()) => {
+                editor_refresh_screen();
+                break;
+            }
         }
     }
 }
